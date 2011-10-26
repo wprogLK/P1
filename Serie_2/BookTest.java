e80e5a41c32043cfde1cb700e565f99b141f7f5b
Urs Gerber <ug.gerber@gmail.com>
2011-10-26 20:01:32 +0200

    Added BookTest methods
    	modified:   Serie_2/BookTest.java

--- Serie_2/BookTest.java
+++ Serie_2/BookTest.java
@@ -1,14 +1,19 @@
+import java.text.ParseException;
+
 
 public class BookTest
 {
 
 	/**
 	 * @param args
+	 * @throws ParseException 
 	 */
-	public static void main(String[] args)
+	public static void main(String[] args) throws ParseException
 	{
-		// TODO Auto-generated method stub
-
+		Book book = new Book();
+		book.input();
+		System.out.println("Book's age: " + book.age() + " days.");
+		System.out.println(book.toString());
 	}
 
 }
