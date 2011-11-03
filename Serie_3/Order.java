2ccbcd29a928536b1b62a1953f61377222801eae
Urs Gerber <ug.gerber@gmail.com>
2011-11-03 10:14:33 +0100

    Stupid five instance variables!
    	modified:   Serie_3/Order.java

--- Serie_3/Order.java
+++ Serie_3/Order.java
@@ -4,7 +4,14 @@ public class Order {
 	private int id;
 	private String customerName;
 	private String customerAddress;
-	private ArrayList<Book> books;
+	private Book bookA;
+	private Book bookB;
+	private Book bookC;
+	private Book bookD;
+	private Book bookE;
+	
+	private int bookCount;
+	private int totalPrice;
 
 	private static int idCounter = 1;
 	private static final int MAX_BOOK_COUNT = 5;
@@ -13,26 +20,55 @@ public class Order {
 		this.id = idCounter++;
 		this.customerName = "";
 		this.customerAddress = "";
-		this.books = new ArrayList<Book>(MAX_BOOK_COUNT);
+		this.bookCount = 0;
+		this.bookA = null;
+		this.bookB = null;
+		this.bookC = null;
+		this.bookD = null;
+		this.bookE = null;
+		this.totalPrice = 0;
 	}
 
 	public Order(String customerName, String customerAddress) {
 		this.id = idCounter++;
 		this.customerName = customerName;
 		this.customerAddress = customerAddress;
-		this.books = new ArrayList<Book>(MAX_BOOK_COUNT);
+		this.bookCount = 0;
+		this.bookA = null;
+		this.bookB = null;
+		this.bookC = null;
+		this.bookD = null;
+		this.bookE = null;
+		this.totalPrice = 0;
 	}
 
 	public void addBook(Book book) {
-		this.books.add(book);
+		switch (this.bookCount)
+		{
+		case 0:
+			this.bookA = book;
+			break;
+		case 1:
+			this.bookB = book;
+			break;
+		case 2:
+			this.bookC = book;
+			break;
+		case 3:
+			this.bookD = book;
+			break;
+		case 4:
+			this.bookE = book;
+			break;
+		default:
+			break;
+		}
+		this.bookCount++;
+		this.totalPrice += book.getPrice();
 	}
 
 	public int getTotalPrice() {
-		int totalPrice = 0;
-		for (Book book : this.books) {
-			totalPrice += book.getPrice();
-		}
-		return totalPrice;
+		return this.totalPrice;
 	}
 
 	public String toString() {
@@ -40,9 +76,17 @@ public class Order {
 
 		sb.append(String.format("Order id: %d, Customer: %s, %s\n", this.id,
 				this.customerName, this.customerAddress));
-		for (Book book : this.books) {
-			sb.append(book.toString() + "\n" );
+		if (this.bookCount == 0)
+			sb.append("No Books in Order!");
+		else 
+		{
+			if (this.bookCount > 0) sb.append(bookA.toString());
+			if (this.bookCount > 1) sb.append(bookB.toString());
+			if (this.bookCount > 2) sb.append(bookC.toString());
+			if (this.bookCount > 3) sb.append(bookD.toString());
+			if (this.bookCount > 4) sb.append(bookE.toString());
 		}
+			
 		sb.append(String.format("Total price: %d CHF\n", this.getTotalPrice()));
 
 		return sb.toString();
