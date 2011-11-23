d096f87a8dd12d933a9d3090a89a4e6ae8aa0d5a
Urs Gerber <ug.gerber@gmail.com>
2011-11-23 21:36:29 +0100

    Weiss nüm was gänderet isch^^
    	modified:   Serie_4/fourInARow/AwesomePlayer.java

--- Serie_4/fourInARow/AwesomePlayer.java
+++ Serie_4/fourInARow/AwesomePlayer.java
@@ -34,7 +34,7 @@ public class AwesomePlayer implements IPlayer
 	private static final int[][] quantifierMap = { { 0, 2, 2, 3, 1,1 }, { 2, 3, 3, 4, 2,2 }, { 4, 4, 4, 5, 3,3 },
 		{ 8, 6, 5, 5, 4,3 },{ 4, 4, 4, 5, 3,3 }, { 2, 3, 3, 4, 2,2 }, { 0, 2, 2, 3, 1,1 } };
 	
-	private static final int MAX_DEPTH = 8;
+	private static final int MAX_DEPTH = 7;
 	private static final int WINNING_SCORE = 1000;
 	private static final int UNDEF_SCORE = 1000000;
 	
