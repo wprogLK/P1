032f082f46b34f5edb0d14b175c2de82a85191e6
Urs Gerber <ug.gerber@gmail.com>
2011-12-12 00:59:44 +0100

    Finished exercise, at least I hope so. Sorting works now. Was a pain in the ass
    	modified:   MergeSort.java

--- Serie_6/MergeSort.java
+++ Serie_6/MergeSort.java
@@ -1,30 +1,61 @@
 public class MergeSort
-{
-
+{	
+	@SuppressWarnings("rawtypes")
 	public static void sort(Comparable[] array)
-	{
-		if (array == null)
-			return;
-		if (array.length < 2)
-			return;
+	{	
+		Comparable[] shadow = new Comparable[array.length];
 		
-		mergeSort(array,0, array.length - 1);
+		mergeSort(array, shadow, 0, array.length - 1);
 	}
 	
-	private static void mergeSort(Comparable[] array, int left, int right)
+	@SuppressWarnings("rawtypes")
+	private static void mergeSort(Comparable[] array, Comparable[] copy, int left, int right)
 	{
 		if (right - left <= 0) return;
 		
 		int center = (left + right) / 2;
 		
-		mergeSort(array, left, center);
-		mergeSort(array, center + 1, right);
+		mergeSort(array, copy, left, center);
+		mergeSort(array, copy, center + 1, right);
 		
-		merge(array, left, center + 1, right);
+		merge(array, copy, left, center + 1, right);
 	}
 	
-	private static void merge(Comparable[] array, int left, int center, int right)
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static void merge(Comparable[] array, Comparable[] copy, int left, int center, int right)
 	{
-		// blabla
+		int endLeft = center - 1;
+		int endRight = right;
+		
+		int leftIdx = left;
+		int rightIdx = center;
+		int idx = left;
+		
+		while (leftIdx <= endLeft && rightIdx <= endRight)
+		{
+			Comparable leftElement = array[leftIdx];
+			Comparable rightElement = array[rightIdx];
+			
+			if (leftElement.compareTo(rightElement) <= 0)
+			{
+				copy[idx] = leftElement;
+				leftIdx++;
+				idx++;
+			}
+			else
+			{
+				copy[idx] = rightElement;
+				rightIdx++;
+				idx++;
+			}
+		}
+		
+		while (leftIdx <= endLeft) { copy[idx] = array[leftIdx]; leftIdx++; idx++; }
+		while (rightIdx <= endRight) { copy[idx] = array[rightIdx]; rightIdx++; idx++; }
+		
+		for (int i = left; i <= endRight; i++)
+		{
+			array[i] = copy[i];
+		}
 	}
 }
