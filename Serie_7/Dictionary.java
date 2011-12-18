ca5d5dd74d7524e5d6a0adc437fe4238e3522be4
Urs Gerber <ug.gerber@gmail.com>
2011-12-18 15:45:39 +0100

    Finished dictionary exercise
    	new file:   Dictionary.java
    	new file:   WordNotFoundException.java

---
+++ Serie_7/Dictionary.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Dictionary
+{
+	private HashMap<String, List<String>> map;
+	
+	public Dictionary()
+	{
+		this.map = new HashMap<String, List<String>>();
+	}
+	
+	public void addTranslations(String key, String[] values)
+	{
+		List<String> newValues = new ArrayList<String>(Arrays.asList(values));
+		
+		if (!map.containsKey(key)) 
+			this.map.put(key, newValues);
+		else
+			(this.map.get(key)).addAll(newValues);	
+	}
+
+	public List<String> translate(String word) throws WordNotFoundException
+	{
+		if (!map.containsKey(word))
+			throw new WordNotFoundException("Word not found!");
+		
+		return map.get(word) ;
+	}
+}
---
+++ Serie_7/WordNotFoundException.java
@@ -0,0 +1,8 @@
+
+public class WordNotFoundException extends Exception
+{
+	public WordNotFoundException(String msg)
+	{
+		super(msg);
+	}
+}
